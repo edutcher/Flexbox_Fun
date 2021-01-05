7114139d4014cfb4f275f0f48e9fdcd502f81cda
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-01-05 18:08:05 -0500

    added align Items

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const direction = document.getElementById('direction');
 const contanier = document.getElementById('container');
 const justify = document.getElementById('justify');
+const align = document.getElementById('align');
 
 direction.addEventListener('change', function() {
     contanier.style.flexDirection = direction.value;
@@ -8,4 +9,8 @@ direction.addEventListener('change', function() {
 
 justify.addEventListener('change', function() {
     contanier.style.justifyContent = justify.value;
+});
+
+align.addEventListener('change', function() {
+    contanier.style.alignItems = align.value;
 });

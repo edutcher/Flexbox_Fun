993e527180e79fe58a5af30a5008cd5c4d48efd2
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-01-05 17:31:24 -0500

    added minor style and justify content

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const direction = document.getElementById('direction');
 const contanier = document.getElementById('container');
+const justify = document.getElementById('justify');
 
 direction.addEventListener('change', function() {
     contanier.style.flexDirection = direction.value;
+});
+
+justify.addEventListener('change', function() {
+    contanier.style.justifyContent = justify.value;
 });
